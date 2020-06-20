b12e817ce92ae97fde88afd09795b7e4debaf1d8
Maeckeul <maeckeulchenais@hotmail.fr>
2020-06-20 16:51:37 +0200

    :lipstick: Burger Menu JS created

--- content/themes/zumba-christele/app/js/app.js
+++ content/themes/zumba-christele/app/js/app.js
@@ -1,6 +1,22 @@
 var app = {
+
   init: function() {
+
     console.log('init');
+
+    app.addEvents();
+  },
+  addEvents: function() {
+
+    $('.ui-button').on('click', app.handleToggleMenu);
+  },
+  handleToggleMenu: function() {
+
+    event.preventDefault();
+
+    console.log('J\'ai clicqué sur le menu')
+
+    $('body').toggleClass('menu-visible');
   }
 };
 
--- content/themes/zumba-christele/footer.php
+++ content/themes/zumba-christele/footer.php
@@ -1,58 +1,57 @@
 </main>
 
-    <footer class="footer">
+<footer class="footer">
 
-      <div class="social">
-        <ul class="social__list">
-          <li class="social__list--item">
-            <a href="#" class="social__list--link">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li class="social__list--item">
-            <a href="#" class="social__list--link">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li class="social__list--item">
-            <a href="#" class="social__list--link">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="footer-logo">
-        <img src="silhouette.png" alt="girl" class="footer-logo__img">
-      </div>
-
-    </footer>
-
-    <!-- Nav 
-    <nav class="nav">
-      <ul class="nav__list">
-        <li class="nav__list--item" href="#">
-          <a href="#" class="nav__list--link">Accueil</a>
-        </li>
-        <li class="nav__list--item" href="#">
-          <a href="#" class="nav__list--link">Planning</a>
-        </li>
-        <li class="nav__list--item" href="#">
-          <a href="#" class="nav__list--link">Médias</a>
-        </li>
-        <li class="nav__list--item" href="#">
-          <a href="#" class="nav__list--link">Tarifs</a>
-        </li>
-        <li class="nav__list--item" href="#">
-          <a href="#" class="nav__list--link">Contacts</a>
-        </li>
-      </ul>
-    </nav>
-    -->
-
+  <div class="social">
+    <ul class="social__list">
+      <li class="social__list--item">
+        <a href="#" class="social__list--link">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+      </li>
+      <li class="social__list--item">
+        <a href="#" class="social__list--link">
+          <i class="fab fa-twitter"></i>
+        </a>
+      </li>
+      <li class="social__list--item">
+        <a href="#" class="social__list--link">
+          <i class="fab fa-instagram"></i>
+        </a>
+      </li>
+    </ul>
   </div>
 
-  <?php wp_footer(); ?>
-  
+  <div class="footer-logo">
+    <img src="silhouette.png" alt="girl" class="footer-logo__img">
+  </div>
+
+</footer>
+
+<!-- Nav 
+<nav class="nav">
+  <ul class="nav__list">
+    <li class="nav__list--item" href="#">
+      <a href="#" class="nav__list--link">Accueil</a>
+    </li>
+    <li class="nav__list--item" href="#">
+      <a href="#" class="nav__list--link">Planning</a>
+    </li>
+    <li class="nav__list--item" href="#">
+      <a href="#" class="nav__list--link">Médias</a>
+    </li>
+    <li class="nav__list--item" href="#">
+      <a href="#" class="nav__list--link">Tarifs</a>
+    </li>
+    <li class="nav__list--item" href="#">
+      <a href="#" class="nav__list--link">Contacts</a>
+    </li>
+  </ul>
+</nav>
+-->
+
+</div>
+
+<script src="js/app.js"></script>
 </body>
 </html>
