7bbf2081ca7ad7ab420c27debf85a45005f2cdca
davy-moreno <davy.moreno@gmail.com>
2020-06-24 11:35:02 +0200

    creation of CPT and loops in the homepage

--- content/themes/zumba-christele/template-parts/front-page/intro.php
+++ content/themes/zumba-christele/template-parts/front-page/intro.php
@@ -1,24 +1,29 @@
 <section class="intro">
 
-<div class="intro--presentation">
-  Christèle, coach sportive à Noirmoutier
-</div>
+  <?php
 
-<div class="intro__bio">
-  <img src="https://scontent-mrs2-1.xx.fbcdn.net/v/t1.0-9/68567523_225419275091169_5685688641431535616_o.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_ohc=YZEUuPblU60AX9EKynV&_nc_ht=scontent-mrs2-1.xx&oh=8f02b064ca5c77539bc82a38530781c3&oe=5F139C20" alt="girl" class="intro__bio--picture">
+    $args = [
+    'orderby' => 'title',
+    'order' => 'ASC',
+    'post_type' => 'introduction',
+    'posts_per_page' => 1,
+    ];
 
-  <p class="intro--content">
-    Lorem, ipsum 
-    dolor sit amet consectetur adipisicing elit. 
-    Qui adipisci sapiente nemo dolorum ea alias 
-    tenetur odit architecto eaque asperiores libero 
-    dolores blanditiis cum suscipit vero, totam, hic 
-    incidunt quisquam esse soluta voluptatum 
-    consequatur accusamus modi? Ipsam officia 
-    temporibus eligendi earum ea sed voluptate, 
-    praesentium cum vitae eius provident laudantium 
-    quis alias.
-  </p>
-</div>
+    $wp_query = new WP_Query($args);
+
+    if ($wp_query->have_posts()): while($wp_query->have_posts()): $wp_query->the_post(); ?>
+
+      
+      <div class="intro--presentation">
+        <?php the_title(); ?>
+      </div>
+
+      <div class="intro__bio">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="girl" class="intro__bio--picture">
+        <div class="intro--content"><?php the_content(); ?></div>
+    
+      </div>
+
+      <?php endwhile; endif; ?>
 
 </section>
