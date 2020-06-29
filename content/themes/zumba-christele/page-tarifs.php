23116ca4eb6c27a00f94960dcbe0d3dc029bfeb7
davy-moreno <davy.moreno@gmail.com>
2020-06-29 15:35:54 +0200

    media.php dynamization

--- content/themes/zumba-christele/page-tarifs.php
+++ content/themes/zumba-christele/page-tarifs.php
@@ -6,10 +6,3 @@ get_header();
 get_template_part('template-parts/page/tarifs');
 
 get_footer(); 
-=======
-<?php get_header(); ?>
-
-
-
-<? get_footer(); ?>
->>>>>>> master
---
+++ content/themes/zumba-christele/template-parts/page/media.php
@@ -0,0 +1,65 @@
+<section class="intro">
+
+<div class="intro--presentation">
+  Médias
+</div>
+
+<div class="intro__bio">
+          
+  <p class="intro--content">
+    Lorem, ipsum 
+    dolor sit amet consectetur adipisicing elit. 
+    Qui adipisci sapiente nemo dolorum ea alias 
+    tenetur odit architecto eaque asperiores libero 
+    dolores blanditiis cum suscipit vero, totam, hic 
+    incidunt quisquam esse soluta voluptatum 
+    consequatur accusamus modi? Ipsam officia 
+    temporibus eligendi earum ea sed voluptate, 
+    praesentium cum vitae eius provident laudantium 
+    quis alias.
+  </p>
+</div>
+
+</section>
+
+<section class="media-grid">
+
+<div class="grid--description">
+  Photos
+</div>
+
+
+
+<div class="media-grid__item">
+  <img src="https://media.istockphoto.com/photos/young-ballerinas-dancing-doing-practice-in-ballet-studio-picture-id1046156520?b=1&k=6&m=1046156520&s=170667a&w=0&h=SZZgi7KyOVMv2icvlBAk4hHdacgqhSMK105phFy-qFI=" class="media-grid__item--picture" alt="image">
+  <img src="https://media.istockphoto.com/photos/professional-dancer-class-dancing-in-dancing-studio-picture-id1170865144?b=1&k=6&m=1170865144&s=170667a&w=0&h=16nwjZyxptQcmz9JKrzTW9MPQguziirONzGAWfZj93Q=" class="media-grid__item--picture" alt="image">
+  <img src="https://images.unsplash.com/photo-1518611012118-696072aa579a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2700&q=80" class="media-grid__item--picture" alt="image">
+  <img src="https://images.unsplash.com/photo-1518622358385-8ea7d0794bf6?ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" class="media-grid__item--picture" alt="image">
+   
+</div>
+
+
+
+        
+</section>
+
+<section class="media-grid">
+
+<div class="grid--description">
+  Vidéos
+</div>
+
+
+
+<div class="media-grid__item">
+  <iframe class="media-grid__item--video" src="https://www.youtube.com/embed/1N-n1KEivbo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe class="media-grid__item--video" src="https://www.youtube.com/embed/8DZktowZo_k" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe class="media-grid__item--video" src="https://www.youtube.com/embed/kC7ILO-gujU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe class="media-grid__item--video" src="https://www.youtube.com/embed/8HpG0l9cLos" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+   
+</div>
+
+
+
+        
+</section>
