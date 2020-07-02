2584992f58402b0796a271bce997cd1af6c438f1
davy-moreno <davy.moreno@gmail.com>
2020-07-02 22:06:03 +0200

    css cleaner

--- content/themes/zumba-christele/template-parts/page/contacts.php
+++ content/themes/zumba-christele/template-parts/page/contacts.php
@@ -10,7 +10,41 @@
         <div class="contacts__form" >
       
           <?= do_shortcode('[contact-form-7 id="95" title="Contact Zumba Christèle"]'); ?>
+
         </div>
+
+        <!--<form class="contacts__form" action="" method="POST">
+
+            <div class="contacts__form--field">
+                <label for="name" class="contacts__form--field--label">Votre nom (obligatoire)</label>
+                <input class="name" type="text" id="name" name="user_name">
+            </div>
+
+            <div class="contacts__form--field">
+                <label for="mail" class="contacts__form--field--label">Votre adresse de messagerie (obligatoire)</label>
+                <input class="mail"  type="email" id="mail" name="user_mail">
+            </div>
+
+            <div class="contacts__form--field">
+                <label for="tel" class="contacts__form--field--label">Votre numéro de téléphone (obligatoire)</label>
+                <input class="tel" type="tel" id="tel" name="user_tel">
+            </div>
+
+            <div class="contacts__form--field">
+                <label for="objet" class="contacts__form--field--label">Objet</label>
+                <input class="objet" type="text" id="objet" name="user_objet">
+            </div>
+
+            <div class="contacts__form--field">
+                <label for="message" class="contacts__form--field--label">Votre message</label>
+                <textarea class="message" id="message" name="user_message"></textarea>
+            </div>
+
+            <div class="contacts__form--button">
+                <button type="submit" class="button">Envoyer</button>
+            </div>
+        
+        </form>-->
 
       </section>
 
