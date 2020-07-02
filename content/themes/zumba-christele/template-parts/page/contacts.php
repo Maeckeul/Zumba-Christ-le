85d0e481cf0f52b1da8d2691646f8e15b0b370da
davy-moreno <davy.moreno@gmail.com>
2020-07-02 11:18:50 +0200

    Dynamization of the price page and creation of the CPT

--- content/themes/zumba-christele/template-parts/page/contacts.php
+++ content/themes/zumba-christele/template-parts/page/contacts.php
@@ -7,45 +7,25 @@
 
       <section class="contacts">
 
-        <!--<form class="contacts__form" action="" method="POST">
-            <div class="contacts__form--field">
-                <label for="name" class="contacts__form--field--label">Votre nom (obligatoire)</label>
-                <input type="text" id="name" name="user_name">
-            </div>
-            <div class="contacts__form--field">
-                <label for="mail" class="contacts__form--field--label">Votre adresse de messagerie (obligatoire)</label>
-                <input type="email" id="mail" name="user_mail">
-            </div>
-            <div class="contacts__form--field">
-                <label for="tel" class="contacts__form--field--label">Votre numéro de téléphone (obligatoire)</label>
-                <input type="tel" id="tel" name="user_tel">
-            </div>
-            <div class="contacts__form--field">
-                <label for="objet" class="contacts__form--field--label">Objet</label>
-                <input type="text" id="objet" name="user_objet">
-            </div>
-            <div class="contacts__form--field">
-                <label for="message" class="contacts__form--field--label">Votre message</label>
-                <textarea id="message" name="user_message"></textarea>
-            </div>
-            <div class="contacts__form--button">
-                <button type="submit" class="button">Envoyer</button>
-            </div>
-        
-        </form>-->
+        <div class="contacts__form" >
+                        
+            <?= do_shortcode('[contact-form-7 id="95" title="Contact Zumba Christèle"]'); ?>
 
-        <?= do_shortcode('[contact-form-7 id="95" title="Contact Zumba Christèle"]'); ?>
-
+        </div>
 
       </section>
+
+      
       <section class="contacts__info">
         <address class="contacts__info--content">
+            
             <div class="mail">
                 Email: zumba-christèle@gmail.com
             </div>
             <div class="tel">
                 Téléphone: 06.25.99.11.91
             </div>
+            
             
         </address>
       </section>
--- content/themes/zumba-christele/template-parts/page/tarifs.php
+++ content/themes/zumba-christele/template-parts/page/tarifs.php
@@ -5,8 +5,29 @@
         </div>
       </section>
 
+      <?php 
+
+        $args =[
+          'orderby' => 'title',
+          'order' => 'DSC',
+          'post_type' => 'price',
+        ];
+
+        $wp_query = new WP_Query($args); ?>
+
       <section class="tarifs">
+        
+        <?php if ($wp_query->have_posts()): while($wp_query->have_posts()): $wp_query->the_post(); ?>
+
         <div class="tarifs__item">
+            <div class="tarifs__item--title"><?php the_title(); ?></div>
+            <div class="tarifs__item--price"><?php the_content(); ?></div>
+        </div>
+
+        <?php endwhile; endif; ?>
+
+
+        <!--<div class="tarifs__item">
             <p class="tarifs__item--title">Séance coaching individuel</p>
             <p class="tarifs__item--price">40 €</p>
             <p class="tarifs__item--description">La séance</p>
@@ -23,10 +44,13 @@
         <div class="tarifs__item">
             <p class="tarifs__item--title">Coaching groupes / entreprises</p>
             <p class="tarifs__item--devis">sur devis</p>
-        </div>
+        </div>-->
+
       </section>
+
       <section class="tarifs__description">
         <p class="tarifs__description--content">
+
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis nam laboriosam nihil corrupti! Voluptates, repellendus?
         </p>
         <p class="tarifs__description--content">
